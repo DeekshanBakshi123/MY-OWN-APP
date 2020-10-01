a4e2cf0fcd305fd95b1b0f610bacbda66ba54318
DeekshanBakshi123 <64910825+DeekshanBakshi123@users.noreply.github.com>
2020-10-01 18:02:31 +0530

    Add files via upload

---
+++ Remix_ Career Explorer/code.js
@@ -0,0 +1,210 @@
+onEvent("button6", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button1", "click", function( ) {
+  setScreen("screen12");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("button8", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:7118d30f-7943-40e2-bb04-868a990e0417");
+});
+onEvent("button7", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:687988e1-51fe-430e-931a-87974d35b76f");
+});
+onEvent("button5", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:e882a0af-3ebf-4835-8a1f-2192f8c42c28");
+});
+onEvent("button9", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:09adf2b6-357c-40c5-afd1-2d8263f505ba");
+});
+onEvent("button4", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button10", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button11", "click", function( ) {
+  setScreen("screen8");
+});
+onEvent("button15", "click", function( ) {
+  setScreen("screen9");
+});
+onEvent("button25", "click", function( ) {
+  setScreen("screen10");
+});
+onEvent("button26", "click", function( ) {
+  setScreen("screen11");
+});
+onEvent("button8", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:7118d30f-7943-40e2-bb04-868a990e0417");
+});
+onEvent("button7", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:687988e1-51fe-430e-931a-87974d35b76f");
+});
+onEvent("button5", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:e882a0af-3ebf-4835-8a1f-2192f8c42c28");
+});
+onEvent("button12", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:9af71523-81e7-4063-87d2-d612731eab5d");
+});
+onEvent("button13", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:f125f58e-c490-4255-bec0-436aed2f78ad");
+});
+onEvent("button14", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:1484ed8c-ec83-4404-9e53-9dff0f45252f");
+});
+onEvent("button24", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:12d76a0e-6bea-42ec-ae8a-d37509bc982e");
+});
+onEvent("button16", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:f97fc3a7-266d-48a1-abe0-ecf9d755f66f");
+});
+onEvent("button17", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:2c4230e4-2caa-477d-a143-12faa0fc256a");
+});
+onEvent("button18", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:15080b3d-882d-4a76-b479-8573c7b2c609");
+});
+onEvent("button19", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:b3c9eb00-145e-4963-b4b7-a323234e5a41");
+});
+onEvent("button20", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:57b38839-f655-41c6-8971-71762a11d0c4");
+});
+onEvent("button21", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:07d6b36e-831d-4feb-9e88-90219190f49f");
+});
+onEvent("button22", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:6b344ebd-4703-4387-aeb9-25e9606b17d4");
+});
+onEvent("button23", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:ee3b503a-46eb-40f2-8ca6-baec53f0db6b");
+});
+onEvent("button28", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:083b27d5-d2d5-4d99-925b-868be68ff330");
+});
+onEvent("button29", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:c68f6425-2d1b-4dad-b980-897feca106b2");
+});
+onEvent("button30", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:dbdf9486-1946-46f2-9644-31f4cbd0adbc");
+});
+onEvent("button31", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:3bae5266-175d-4b7a-bfe9-89b666a52403");
+});
+onEvent("button32", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:d461cbc2-9f76-4c13-b9f6-44ebbb8cd141");
+});
+onEvent("button33", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:37186017-956e-45b1-8c19-fbf6468563eb");
+});
+onEvent("button34", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:e9ac3b1b-2379-46cd-a247-c8b89cb93ac5");
+});
+onEvent("button35", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:068f5515-e649-4e25-b8c1-7c45c77b1242");
+});
+onEvent("button36", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:65a47730-d269-48df-ae4c-af4c157ac515");
+});
+onEvent("button37", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:8811886b-053a-46a0-bc4e-8d86cbee3561");
+});
+onEvent("button38", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:96143f9f-7adc-485d-a044-3434b07d9c28");
+});
+onEvent("button39", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:2f7436ed-604c-4945-8150-6135dda4cc2c");
+});
+onEvent("button40", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:703f1b8c-eb0d-4a1f-abf3-4f6557bcfe11");
+});
+onEvent("button41", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:39499e13-c1be-4b91-9404-8d77beda65db");
+});
+onEvent("button42", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:a88d0920-17b6-4743-9941-a327d9e8b10c");
+});
+onEvent("button43", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:48b6c9bd-df58-4850-bb29-66ba86eaabed");
+});
+onEvent("button44", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:e286cbb1-ab9d-429f-b29b-23faaa0e2955");
+});
+onEvent("button45", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:a2fc4d8e-516c-41c1-93ac-bcd877e995d0");
+});
+onEvent("button46", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:e63988ef-b8c9-4897-821d-c67107802676");
+});
+onEvent("button47", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:3fd985df-526f-47ae-a38b-d2657ced0731");
+});
+onEvent("button48", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:6b88e5d3-a818-42b8-8907-045d8b12eb3b");
+});
+onEvent("button49", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:8eaa4b0d-efbf-4622-9e2a-1948f9782f82");
+});
+onEvent("button50", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:d4770f40-026f-4759-a6dd-8b9f4b7616ef");
+});
+onEvent("button51", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:5f98a2df-6cd5-4448-9e14-b54924e6bd3b");
+});
+onEvent("button52", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:4c8302dd-6d9c-4d3f-b392-7544677493b5");
+});
+onEvent("button53", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:d40df55a-98e3-4315-9efa-e3594b955fea");
+});
+onEvent("button60", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button54", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button61", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button62", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button63", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button64", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button65", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button67", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button66", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button68", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button55", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:801cfad5-89f1-4063-98d5-78bdb4fbc994");
+});
+onEvent("button56", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:c94f2aad-14d6-445b-bb85-d762d058c1b1");
+});
+onEvent("button57", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:40618a49-5ed0-4a55-8e2b-61b247277052");
+});
+onEvent("button58", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:c6e490a4-3ee6-4391-86da-650c9bb5e21f");
+});
+onEvent("button59", "click", function( ) {
+  open("https://documentcloud.adobe.com/link/track?uri=urn:aaid:scds:US:2f5cae29-38ac-4082-835f-db838160465c");
+});
